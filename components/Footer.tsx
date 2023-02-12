bc14c1d08a1390ccf0b9e5daa4d9736a722fa55d
bPoliar <bpoliar01@gmail.com>
2023-02-12 10:53:11 +0500

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="flex flex-col items-center">
+        <ul className="text-2xl p-4 font-mono">
+          <li>
+            <a href="https://github.com/user6879">Github</a>
+          </li>
+        </ul>
+      </footer>
+    );
+  };
+  
+  export default Footer;
