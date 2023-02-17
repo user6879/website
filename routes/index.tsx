c037b76db331797875aaa7bf3904ec755cd6032d
user6879 <bpoliar01@gmail.com>
2023-02-17 22:31:02 +0500

    main page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,31 +1,41 @@
 import { Head } from "$fresh/runtime.ts";
-import Footer from "../components/Footer.tsx";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>user6879 Website</title>
-    </Head>
-    <div className="justify-items-center items-center bg-black font-mono  text-white h-screen flex flex-col justify-between text-center">
-        <div class="">
-          <h1 className="text-5xl py-6 ">
-            user6879
-          </h1>
-          <h2 className="text-3xl">
-            About me.
-          </h2>
-          <div>
-            <p className="bg-black-200 text-2xl justify-self-auto px-96 py-6 mx-10 flex">
-              Hi there. I'm 17 y.o. TypeScript coder from Uzbekistan.
-              Currently learning TypeScript for Web development.
-              You can find my projects on my Github page.
-            </p>
+      <Head>
+        <title>user6879 - Website</title>
+        <meta name="description" content="15 years old developer from Russia" />
+        <link rel="shortcut icon" href="../static/favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" href="/globals.css" type="text/css" />
+      </Head>
+
+      <div className="bg-black text-white min-h-screen flex flex-col justify-between">
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col justify-center items-center w-max">
+            <img
+              src="/avatar.png"
+              class="w-52 h-52 rounded-full m-2 border-2 border-gray-800"
+              alt="Avatar"
+            />
+
+            <div className="flex flex-col text-center flex-wrap items-center justify-center p-2 h-full">
+              <h1 className="text-3xl">
+                Hi there!
+              </h1>
+
+              <p>
+                I'm user6879 a.k.a. akumaru.
+                <br />
+                I'm 17 y.o. coder from Uzbekistan.
+                <br />
+                Currently learning TypeScript for web development.
+              </p>
+            </div>
+            <a className="my-3" href="https://github.com/user6879">GitHub</a>
           </div>
         </div>
-        <Footer></Footer>
-    </div>
-    
+      </div>
     </>
   );
 }
